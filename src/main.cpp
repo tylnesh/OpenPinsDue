79c2b0bb6f12cbec43ae2067bdee41efc344619b
Michal Kohutek <michal@kohutek.eu>
2022-07-24 17:35:14 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <Arduino.h>
 #include <ArduinoJson.hpp>
 
-const char* msg = "Hello";
+struct GameStatus{
+  int round;
+  int ballsThrown;
+  int score;
+  bool pinState[9];
+};
+
+  const char* msg = "Hello";
+
 
 void setup() {
 Serial.begin(9600);
-
 }
 
 void loop() {
-  Serial.println(msg);
-  delay(1000);
-}
+  Serial.write(msg,sizeof(msg)+1);git
